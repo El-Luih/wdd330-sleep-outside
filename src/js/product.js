d1949fdee38961e734bd31aae630eb01162806b1
El-Luih <luisalejandro632@gmail.com>
2025-09-14 02:04:13 -0600

    Added dynamic product details building.

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,24 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const detailContainer = document.getElementById("detail-container");
+const productID = getParam("product");
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
+const productDetails = new ProductDetails(
+  productID,
+  dataSource,
+  detailContainer,
+);
+productDetails.init();
+/* add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+add listener to Add to Cart button
 document
   .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .addEventListener("click", addToCartHandler);*/
